76541c43e27e6fe6704847fe04fa1e8ac7724fc4
MaxX <maxx-f@mail.ru>
2022-08-26 09:23:22 +0300

    Add DZ_001

---
+++ DZ_001/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] CreateMatrix(int rowCount, int columnCount, int min, int max)
+{
+    double[,] array = new double[rowCount, columnCount];
+    for (int row = 0; row < rowCount; row++)
+    {
+        for (int column = 0; column < columnCount; column++)
+        {
+            array[row, column] = new Random().Next(min, max+1);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(double[,] arr)
+{
+    for (int row = 0; row < arr.GetLength(0); row++)
+    {
+        for (int column = 0; column < arr.GetLength(1); column++)
+        {
+            Console.Write($"{arr[row, column]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] matrix = CreateMatrix(3, 4, 0, 99);
+PrintMatrix(matrix);
