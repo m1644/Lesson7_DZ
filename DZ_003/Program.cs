50daba3984faa8299c5cf89ead451cb645d5b793
MaxX <maxx-f@mail.ru>
2022-08-26 11:08:34 +0300

    Add DZ_003

---
+++ DZ_003/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] CreateMatrix(int rowCount, int columnCount, int min, int max)
+{
+    int[,] array = new int[rowCount, columnCount];
+    for (int row = 0; row < rowCount; row++)
+    {
+        for (int column = 0; column < columnCount; column++)
+        {
+            array[row, column] = new Random().Next(min, max+1);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int row = 0; row < arr.GetLength(0); row++)
+    {
+        for (int column = 0; column < arr.GetLength(1); column++)
+        {
+            Console.Write($"{arr[row, column]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindAverage(int[,] array)
+{
+    double average = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            average += array[i, j];
+        }
+        average = Math.Round(average / array.GetLength(0), 2, MidpointRounding.ToZero);
+        Console.Write($"{average} \t");
+        average = 0;
+    }
+}
+
+int[,] matrix = CreateMatrix(4, 3, 0, 99);
+PrintMatrix(matrix);
+Console.WriteLine();
+FindAverage(matrix);
